2f7c8371ea82898e33a3a816bf431541bdad529a
Yuborama <davidjohan2@hotmail.com>
2021-07-15 14:24:46 -0500

    init proyect

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import {FC} from 'react'
+
+interface Props {
+    
+}
+
+const index: FC<Props> = () => {
+    return (
+        <div>
+            <p>hola</p>
+        </div>
+    )
+}
+
+export default index
