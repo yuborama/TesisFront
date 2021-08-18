ea971474c46519eb564bb3cd9c5769cd55813499
Yuborama <davidjohan2@hotmail.com>
2021-08-17 21:36:39 -0600

    feature translate bars

--- src/components/grafics/index.tsx
+++ src/components/grafics/index.tsx
@@ -2,7 +2,7 @@ import styled from "@emotion/styled";
 import { css } from "@emotion/react";
 import React, { FC, useState } from "react";
 //@ts-ignore
-import { Chart, Ticks, Layer, Bars, Transform,Dots,Labels } from "rumble-charts";
+import {  Chart,  Ticks,  Layer,  Bars,  Transform,  Dots,  Labels,} from "rumble-charts";
 
 type GraficProps = {
   series: {
@@ -11,11 +11,25 @@ type GraficProps = {
   }[];
 };
 
+const series = [
+  {
+    name: "Top news",
+    data: [14, 12,5,19],
+  },
+  {
+    name: "Word news",
+    data: [13, 41,34,67],
+  }
+];
+
+const barWidth = 80;
+const barOffset = 20;
+
 const GraficComponent: FC<GraficProps> = (props) => {
-  const { series } = props;
+  // const { series } = props;
   return (
     <>
-      <div style={{ fontFamily: "sans-serif", fontSize: "0.8em" }}>
+      {/* <div style={{ fontFamily: "sans-serif", fontSize: "0.8em" }}>
         <Chart width={600} height={250} series={series}>
           <Layer width="95%" height="85%" position="center bottom">
             <Dots />
@@ -36,9 +50,116 @@ const GraficComponent: FC<GraficProps> = (props) => {
             <Bars combined={true} groupPadding="3%" innerPadding="0.5%" />
           </Transform>
         </Chart>
-      </div>
-
+      </div> */}
       <div style={{ fontFamily: "sans-serif", fontSize: "0.8em" }}>
+        <Chart width={600} height={300} series={series} minY={0}>
+          <Layer width="80%" height="90%" position="top center">
+            <Ticks
+              axis="y"
+              lineLength="100%"
+              lineVisible={true}
+              lineStyle={{ stroke: "lightgray" }}
+              labelStyle={{
+                textAnchor: "end",
+                dominantBaseline: "middle",
+                fill: "lightgray",
+              }}
+              labelAttributes={{ x: -5 }}
+            />
+            {/* <Ticks
+              axis="x"
+              label={({series}) => series.name}
+              labelStyle={{
+                textAnchor: "middle",
+                dominantBaseline: "text-before-edge",
+                fill: "lightgray",
+              }}
+              labelAttributes={{ y: 3 }}
+            /> */}
+            <Transform method="transpose">
+              <Bars
+                barAttributes={{
+                  onMouseMove: (e:any) => (e.target.style.opacity = 0.5),
+                  onMouseLeave: (e:any) => (e.target.style.opacity = 1),
+                }}
+                barStyle={({ seriesIndex, props }: any) => {
+                  const len = props.series.length;
+                  const offset = -((1 - len) / 2 + seriesIndex) * 25;
+                  return {
+                    // opacity: 0.5,
+                    transform: `translateX(${offset}px)`,
+                  };
+                }}
+              />
+            </Transform>
+            {/* <Bars
+              seriesIndex={3}
+              barWidth={barWidth}
+              colors={["yellow"]}
+              style={{ transform: `translateX(${barOffset * 2}px)` }}
+              barStyle={{
+                // hover:,
+                fillOpacity: 0.5,
+                transition: "all 250ms",
+              }}
+            />
+            <Bars
+              seriesIndex={2}
+              barWidth={barWidth}
+              colors={["red"]}
+              style={{ transform: `translateX(${barOffset}px)` }}
+            />
+            <Bars
+              seriesIndex={1}
+              barWidth={barWidth}
+              colors={["green"]}
+              barAttributes={{
+                onMouseMove: (e: any) => (e.target.style.fillOpacity = 0.5),
+              }}
+            />
+            <Bars
+              seriesIndex={0}
+              barWidth={barWidth}
+              colors={["blue"]}
+              style={{ transform: `translateX(-${barOffset}px)` }}
+            /> */}
+          </Layer>
+        </Chart>
+      </div>
+      {/* <Chart width={600} height={250} series={series} minY={0}>
+        <Layer>
+          <Ticks
+            axis="x"
+            label={({ index, props }: any) => props.series[index].name}
+            labelStyle={{
+              textAnchor: "middle",
+              dominantBaseline: "text-before-edge",
+              fill: "lightgray",
+            }}
+            labelAttributes={{ y: 3 }}
+          />
+        </Layer>
+        <Bars
+          seriesIndex={3}
+          barWidth={barWidth}
+          colors={["yellow"]}
+          style={{ transform: `translateX(${barOffset * 2}px)` }}
+        />
+        <Bars
+          seriesIndex={2}
+          barWidth={barWidth}
+          colors={["red"]}
+          style={{ transform: `translateX(${barOffset}px)` }}
+        />
+        <Bars seriesIndex={1} barWidth={barWidth} colors={["green"]} />
+        <Bars
+          seriesIndex={0}
+          barWidth={barWidth}
+          colors={["blue"]}
+          style={{ transform: `translateX(-${barOffset}px)` }}
+        />
+      </Chart> */}
+      {/* <div style={{ fontFamily: "sans-serif", fontSize: "0.8em" }}>
         <Chart width={600} height={300} series={series} minY={0}>
           <Layer width="80%" height="90%">
             <Ticks
@@ -53,20 +174,11 @@ const GraficComponent: FC<GraficProps> = (props) => {
               }}
               labelAttributes={{ x: -5 }}
             />
-            <Ticks
-              axis="x"
-              label={({ index, props }: any) => props.series[index].name}
-              labelStyle={{
-                textAnchor: "middle",
-                dominantBaseline: "text-before-edge",
-                fill: "lightgray",
-              }}
-              labelAttributes={{ y: 3 }}
-            />
+            
             <Bars groupPadding="3%" innerPadding="0.5%" />
           </Layer>
         </Chart>
-      </div>
+      </div> */}
     </>
   );
 };
