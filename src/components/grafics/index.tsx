511eebed4eeef78c1fa6750d8c3266340ca7abf2
Yuborama <davidjohan2@hotmail.com>
2021-08-24 12:27:17 -0600

    delete comment grafics

--- src/components/grafics/index.tsx
+++ src/components/grafics/index.tsx
@@ -1,9 +1,6 @@
-import styled from "@emotion/styled";
-import { css } from "@emotion/react";
 import React, { Dispatch, FC, SetStateAction, useState } from "react";
 //@ts-ignore
 import { Chart, Ticks, Layer, Bars, Transform } from "rumble-charts";
-import { cursorTo } from "readline";
 import LegendComponent from "../legend";
 
 type GraficProps = {
@@ -30,39 +27,12 @@ const series = [
 const name = ["base", "pozo adasdasdadasdasdasd", "your news"];
 const color = ["red", "blue", "yellow"];
 
-const barWidth = 80;
-const barOffset = 20;
-
 const GraficComponent: FC<GraficProps> = (props) => {
   const [title, setTitle] = useState("");
   const [active, setActive] = useState<number[]>([]);
-  // const [filter, setFilter] = useState(-1);
-  // const { series } = props;
   return (
     <>
       <div title={title}>
-        {/* <div style={{ fontFamily: "sans-serif", fontSize: "0.8em" }}>
-        <Chart width={600} height={250} series={series}>
-          <Layer width="95%" height="85%" position="center bottom">
-            <Dots />
-            <Labels
-              label={({ point }:any) => "y=" + point.y}
-              dotStyle={{
-                textAnchor: "middle",
-                dominantBaseline: "text-after-edge",
-                fontFamily: "sans-serif",
-                fontSize: "0.8em",
-              }}
-              labelAttributes={{
-                y: -4,
-              }}
-            />
-          </Layer>
-          <Transform method="stack">
-            <Bars combined={true} groupPadding="3%" innerPadding="0.5%" />
-          </Transform>
-        </Chart>
-      </div> */}
         <div style={{ fontFamily: "sans-serif", fontSize: "0.8em" }}>
           <Chart
             width={600}
@@ -105,9 +75,6 @@ const GraficComponent: FC<GraficProps> = (props) => {
               <Transform method="transpose">
                 <Bars
                   colors={color.filter((e, index) => {
-                    // console.log('revisando', active.length === 0? e: !active.includes(index));
-                    // console.log('index', index);
-                    // console.log('element', e);
                     return active.length === 0 ? e : !active.includes(index);
                   })}
                   barAttributes={{
@@ -124,7 +91,13 @@ const GraficComponent: FC<GraficProps> = (props) => {
                         );
                         return(series[subcodigo].data[data])
                       };
-                      setTitle(String(poiterData(e,series)));
+                      const over = series.map((e) => ({
+                        ...e,
+                        data: e.data.filter((_, index) =>
+                          active.length === 0 ? true : !active.includes(index)
+                        ),
+                      }))
+                      setTitle(String(poiterData(e,over)));
                       e.target.style.cursor = "pointer";
                       e.target.style.opacity = 0.5;
                     },
@@ -136,130 +109,24 @@ const GraficComponent: FC<GraficProps> = (props) => {
                   className={"bar"}
                   barStyle={({ seriesIndex, props }: any) => {
                     const len = props.series.length;
-                    const offset = -((1 - len) / 2 + seriesIndex) * 25;
+                    const offset = -((1 - len) / 2 + (seriesIndex)) * 25;
+                    // console.log('seriesIndex props', {series:seriesIndex, props:props,offset:offset,len:len});
                     return {
-                      // opacity: 0.5,
                       transform: `translateX(${offset}px)`,
+                      // zIndex:  `${(0-seriesIndex)}`,
                     };
                   }}
                 />
               </Transform>
-              {/* <Bars
-              seriesIndex={3}
-              barWidth={barWidth}
-              colors={["yellow"]}
-              style={{ transform: `translateX(${barOffset * 2}px)` }}
-              barStyle={{
-                // hover:,
-                fillOpacity: 0.5,
-                transition: "all 250ms",
-              }}
-            />
-            <Bars
-              seriesIndex={2}
-              barWidth={barWidth}
-              colors={["red"]}
-              style={{ transform: `translateX(${barOffset}px)` }}
-            />
-            <Bars
-              seriesIndex={1}
-              barWidth={barWidth}
-              colors={["green"]}
-              barAttributes={{
-                onMouseMove: (e: any) => (e.target.style.fillOpacity = 0.5),
-              }}
-            />
-            <Bars
-              seriesIndex={0}
-              barWidth={barWidth}
-              colors={["blue"]}
-              style={{ transform: `translateX(-${barOffset}px)` }}
-            /> */}
             </Layer>
           </Chart>
-          {/* <div >
-    <Dots />
-    <Labels 
-      label={({_,index}:any) => ('y=' + name[index])}
-      dotStyle={{
-        textAnchor: 'middle',
-        dominantBaseline: 'text-after-edge',
-        fontFamily: 'sans-serif',
-        fontSize: '0.8em'
-      }}
-      labelAttributes={{
-        y: -4
-      }}
-    />
-        </div> */}
-
           <LegendComponent
             color={color}
             name={name}
             active={active}
             setActive={setActive}
           />
-          {/* <div><div 
-            onClick={()=>setFilter(-1)} 
-            style={{
-              display: "flex",
-              alignItems:"center",
-              // justifyContent:"center"
-            }}>
-                
-                <span>todos los datos</span>
-              </div>
-            {name.map((x,i)=> <div 
-            onClick={()=>setFilter(i)} 
-            style={{
-              display: "flex",
-              alignItems:"center",
-              // justifyContent:"center"
-            }}>
-                <div style={{
-                height:'10px', 
-                width:'10px',
-                borderRadius:'50%',
-                backgroundColor:color[i]
-                }}></div>
-                <span>{x}</span>
-              </div>
-            )}
-        </div> */}
         </div>
-        {/* <Chart width={600} height={250} series={series} minY={0}>
-        <Layer>
-          <Ticks
-            axis="x"
-            label={({ index, props }: any) => props.series[index].name}
-            labelStyle={{
-              textAnchor: "middle",
-              dominantBaseline: "text-before-edge",
-              fill: "lightgray",
-            }}
-            labelAttributes={{ y: 3 }}
-          />
-        </Layer>
-        <Bars
-          seriesIndex={3}
-          barWidth={barWidth}
-          colors={["yellow"]}
-          style={{ transform: `translateX(${barOffset * 2}px)` }}
-        />
-        <Bars
-          seriesIndex={2}
-          barWidth={barWidth}
-          colors={["red"]}
-          style={{ transform: `translateX(${barOffset}px)` }}
-        />
-        <Bars seriesIndex={1} barWidth={barWidth} colors={["green"]} />
-        <Bars
-          seriesIndex={0}
-          barWidth={barWidth}
-          colors={["blue"]}
-          style={{ transform: `translateX(-${barOffset}px)` }}
-        />
-      </Chart> */}
       </div>
     </>
   );
