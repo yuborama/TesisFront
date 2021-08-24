20dcd4edda4605316b3839cfde487a9ab181a68c
Yuborama <davidjohan2@hotmail.com>
2021-08-24 12:53:49 -0600

    3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,4 @@
-import CardFile from "@Src/components/cardfile";
-import DragDrogComponent from "@Src/components/dragdrop";
-import GraficComponent from "@Src/components/grafics";
-import AtomInput from "@Src/components/Input";
-import ModalComponent from "@Src/components/modal";
+
 import FileBaseComponent from "@Src/components/organisms/filebase";
 import { FC } from "react";
 
