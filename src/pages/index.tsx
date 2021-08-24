f0d8661f1c2df9cdd775849917a1e4e2b4c74a53
Yuborama <davidjohan2@hotmail.com>
2021-08-24 12:50:35 -0600

    2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,25 @@
-import {FC} from 'react'
+import CardFile from "@Src/components/cardfile";
+import DragDrogComponent from "@Src/components/dragdrop";
+import GraficComponent from "@Src/components/grafics";
+import AtomInput from "@Src/components/Input";
+import ModalComponent from "@Src/components/modal";
+import FileBaseComponent from "@Src/components/organisms/filebase";
+import { FC } from "react";
 
-interface Props {
-    
-}
+interface Props {}
+
 
 const index: FC<Props> = () => {
-    return (
-        <div>
-            <p>hola</p>
-        </div>
-    )
-}
+  return (
+    <>
+    <FileBaseComponent/>
+      {/* <DragDrogComponent />
+      <CardFile text="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"/>
+      <CardFile text="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"/> */}
+      {/* <AtomInput/>
+      <GraficComponent /> */}
+    </>
+  );
+};
 
-export default index
+export default index;
